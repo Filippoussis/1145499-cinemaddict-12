c951ac8e338ab3db93b10d53e6f1e5fbb92893b5
Filippoussis <filippoussis@iMac-AndrosPhilippos.local>
2020-07-31 15:19:48 +0500

    Создал в src папку view и добавил туда файлы модулей

---
+++ src/view/count.js
@@ -0,0 +1,9 @@
+/**
+ * создает шаблон блока с общим количеством фильмов
+ * @return {string} возвращает HTML-строку в ввиде кода HTML
+ */
+export const createTotalNumberFilmsTemplate = () => {
+  return (
+    `<p>130 291 movies inside</p>`
+  );
+};
