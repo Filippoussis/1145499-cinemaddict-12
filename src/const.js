33d244ee187cadcaf195718d2e2fb32156bbdc32
Filippoussis <filippoussis@iMacAndhilippos.planet-a.ru>
2020-08-08 20:19:52 +0500

    Создал файл const.js и поместил в него константные данные

---
+++ src/const.js
@@ -0,0 +1,72 @@
+export const NAMES = [
+  `The Dance of Life`,
+  `Sagebrush Trail`,
+  `The Man with the Golden Arm`,
+  `Santa Claus Conquers the Martians`,
+  `Popeye the Sailor Meets Sindbad the Sailor`,
+  `The Great Flamarion`,
+  `Made for Each Other`,
+];
+
+export const POSTERS = [
+  `the-dance-of-life.jpg`,
+  `sagebrush-trail.jpg`,
+  `the-man-with-the-golden-arm.jpg`,
+  `santa-claus-conquers-the-martians.jpg`,
+  `popeye-meets-sinbad.png`,
+  `the-great-flamarion.jpg`,
+  `made-for-each-other.png`,
+];
+
+export const DESCRIPTION = [
+  `Lorem ipsum dolor sit amet, consectetur adipiscing elit.`,
+  `Cras aliquet varius magna, non porta ligula feugiat eget.`,
+  `Fusce tristique felis at fermentum pharetra.`,
+  `Aliquam id orci ut lectus varius viverra.`,
+  `Nullam nunc ex, convallis sed finibus eget, sollicitudin eget ante.`,
+  `Phasellus eros mauris, condimentum sed nibh vitae, sodales efficitur ipsum.`,
+  `Sed blandit, eros vel aliquam faucibus, purus ex euismod diam, eu luctus nunc ante ut dui.`,
+  `Sed sed nisi sed augue convallis suscipit in sed felis.`,
+  `Aliquam erat volutpat.`,
+  `Nunc fermentum tortor ac porta dapibus.`,
+  `In rutrum ac purus sit amet tempus.`,
+];
+
+export const NumberSentencesOfDescription = {
+  MIN: 1,
+  MAX: 5,
+};
+
+export const Rating = {
+  MIN: 0,
+  MAX: 10,
+};
+
+export const Year = {
+  MIN: 1920,
+  MAX: 2020,
+};
+
+export const GENRES = [
+  `Drama`,
+  `Mystery`,
+  `Comedy`,
+  `Musical`,
+  `Western`,
+  `Cartoon`,
+];
+
+export const NumberGenres = {
+  MIN: 1,
+  MAX: 3,
+};
+
+export const Comments = {
+  MIN: 0,
+  MAX: 5,
+};
+
+export const UserFilmsCount = {
+  MIN: 0,
+  MAX: 50,
+};
