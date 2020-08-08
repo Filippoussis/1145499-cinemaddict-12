078bb3c9ccd7d530d71431b7dad10e6d051224fc
Filippoussis <filippoussis@iMacAndhilippos.planet-a.ru>
2020-08-08 20:24:56 +0500

    Обучаем шаблон фильтра принимать данные

--- src/view/filter.js
+++ src/view/filter.js
@@ -1,15 +1,19 @@
 /**
  * создает шаблон меню фильтра
+ * @param {object} filters - объект со значением полей фильтров
  * @return {string} возвращает HTML-строку в ввиде кода HTML
  */
-export const createFilterTemplate = () => {
+export const createFilterTemplate = (filters) => {
+
+  const {watchlist, watched, favorites} = filters;
+
   return (
     `<nav class="main-navigation">
       <div class="main-navigation__items">
         <a href="#all" class="main-navigation__item main-navigation__item--active">All movies</a>
-        <a href="#watchlist" class="main-navigation__item">Watchlist <span class="main-navigation__item-count">13</span></a>
-        <a href="#history" class="main-navigation__item">History <span class="main-navigation__item-count">4</span></a>
-        <a href="#favorites" class="main-navigation__item">Favorites <span class="main-navigation__item-count">8</span></a>
+        <a href="#watchlist" class="main-navigation__item">Watchlist <span class="main-navigation__item-count">${watchlist}</span></a>
+        <a href="#history" class="main-navigation__item">History <span class="main-navigation__item-count">${watched}</span></a>
+        <a href="#favorites" class="main-navigation__item">Favorites <span class="main-navigation__item-count">${favorites}</span></a>
       </div>
       <a href="#stats" class="main-navigation__additional">Stats</a>
     </nav>`
