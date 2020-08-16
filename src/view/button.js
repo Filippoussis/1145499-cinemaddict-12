e530828322b2fa11adbc6c8774eda829adb669bb
Filippoussis <filippoussis@iMac-AndrosPhilippos.local>
2020-08-16 17:14:50 +0500

    Переименовал button в show-more-button и реализовал компонент через класс

--- src/view/button.js
+++
@@ -1,9 +0,0 @@
-/**
- * создает шаблон кнопки "Show more"
- * @return {string} возвращает HTML-строку в ввиде кода HTML
- */
-export const createShowMoreButtonTemplate = () => {
-  return (
-    `<button class="films-list__show-more">Show more</button>`
-  );
-};
