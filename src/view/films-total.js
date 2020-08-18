0c52ded5523d987c47ca382fb0e4a1568dbe4e0a
Filippoussis <filippoussis@iMacAndhilippos.planet-a.ru>
2020-08-18 17:23:07 +0500

    Реализовал логику показа количества фильмов в зависимости от длины массива карточек

--- src/view/films-total.js
+++ src/view/films-total.js
@@ -2,23 +2,25 @@ import {createElement} from "../utils.js";
 
 /**
  * создает шаблон блока с общим количеством фильмов
+ * @param {Array} cards - массив объектов карточек фильмов
  * @return {string} возвращает HTML-строку в ввиде кода HTML
  */
-const createFilmsTotalTemplate = () => {
+const createFilmsTotalTemplate = (cards) => {
   return (
     `<section class="footer__statistics">
-      <p>130 291 movies inside</p>
+      <p>${cards.length} movies inside</p>
     </section>`
   );
 };
 
 export default class FilmsTotal {
-  constructor() {
+  constructor(cards) {
+    this._cards = cards;
     this._element = null;
   }
 
   _getTemplate() {
-    return createFilmsTotalTemplate();
+    return createFilmsTotalTemplate(this._cards);
   }
 
   getElement() {
